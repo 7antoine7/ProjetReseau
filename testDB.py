b9fc8df4b42a1ee7b52fd57b64e7bd06efcf97fa
Antoine Leroux <antoineleroux31@gmail.com>
2020-02-14 11:23:45 -0500

    Premier Test d'envoi de donnée dans la DB
     On branch master
    Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   testDB.py

---
+++ testDB.py
@@ -0,0 +1,110 @@
+import os
+import sys
+import time
+import uuid
+import json
+import logging
+import argparse
+from AWSIoTPythonSDK.core.greengrass.discovery.providers import DiscoveryInfoProvider
+from AWSIoTPythonSDK.core.protocol.connection.cores import ProgressiveBackOffCore
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+from AWSIoTPythonSDK.exception.AWSIoTExceptions import DiscoveryInvalidRequestException
+
+MAX_DISCOVERY_RETRIES = 10
+
+def customOnMessage(message):
+    print('Received message on topic %s: %s\n' % (message.topic, message.payload))
+
+rootCAPath = "~/certs/AmazonRootCA1.pem"
+certificatePath = "~/certs/device.pem.crt"
+privateKeyPath = "~/certs/private.pem.key"
+host = "a1botgu2gaco6r-ats.iot.us-west-2.amazonaws.com"
+thingName = "pine64"
+topic = "iot/topic"
+clientId = thingName
+
+
+
+# Discover GGCs
+discoveryInfoProvider = DiscoveryInfoProvider()
+discoveryInfoProvider.configureEndpoint(host)
+discoveryInfoProvider.configureCredentials(rootCAPath, certificatePath, privateKeyPath)
+discoveryInfoProvider.configureTimeout(10)  # 10 sec
+
+retryCount = MAX_DISCOVERY_RETRIES
+discovered = False
+groupCA = None
+coreInfo = None
+while retryCount != 0:
+    try:
+        discoveryInfo = discoveryInfoProvider.discover(thingName)
+        caList = discoveryInfo.getAllCas()
+        coreList = discoveryInfo.getAllCores()
+
+        # We only pick the first ca and core info
+        groupId, ca = caList[0]
+        coreInfo = coreList[0]
+        print("Discovered GGC: %s from Group: %s" % (coreInfo.coreThingArn, groupId))
+
+        print("Now we persist the connectivity/identity information...")
+        groupCA = GROUP_CA_PATH + groupId + "_CA_" + str(uuid.uuid4()) + ".crt"
+        if not os.path.exists(GROUP_CA_PATH):
+            os.makedirs(GROUP_CA_PATH)
+        groupCAFile = open(groupCA, "w")
+        groupCAFile.write(ca)
+        groupCAFile.close()
+
+        discovered = True
+        print("Now proceed to the connecting flow...")
+        break
+    except DiscoveryInvalidRequestException as e:
+        print("Invalid discovery request detected!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+        print("Stopping...")
+        break
+
+        if not discovered:
+            print("Discovery failed after %d retries. Exiting...\n" % (MAX_DISCOVERY_RETRIES))
+    sys.exit(-1)
+
+# Iterate through all connection options for the core and use the first successful one
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureCredentials(groupCA, privateKeyPath, certificatePath)
+myAWSIoTMQTTClient.onMessage = customOnMessage
+
+connected = False
+for connectivityInfo in coreInfo.connectivityInfoList:
+    currentHost = connectivityInfo.host
+    currentPort = connectivityInfo.port
+    print("Trying to connect to core at %s:%d" % (currentHost, currentPort))
+    myAWSIoTMQTTClient.configureEndpoint(currentHost, currentPort)
+    try:
+        myAWSIoTMQTTClient.connect()
+        connected = True
+        break
+    except BaseException as e:
+        print("Error in connect!")
+        print("Type: %s" % str(type(e)))
+        print("Error message: %s" % e.message)
+
+if not connected:
+    print("Cannot connect to core %s. Exiting..." % coreInfo.coreThingArn)
+    sys.exit(-2)
+
+    # Successfully connected to the core
+if args.mode == 'both' or args.mode == 'subscribe':
+    myAWSIoTMQTTClient.subscribe(topic, 0, None)
+time.sleep(2)
+
+loopCount = 0
+while True:
+    message = {}
+    message['rnd'] = 12
+    message['TimeStamp'] = loopCount
+    messageJson = json.dumps(message)
+    myAWSIoTMQTTClient.publish(topic, messageJson, 0)
+    if args.mode == 'publish':
+        print('Published topic %s: %s\n' % (topic, messageJson))
+    loopCount += 1
+    time.sleep(1)
